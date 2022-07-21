78cabfbab86b791c87d1fa9fa12410168845b197
gitsaugat <letnotsay@gmail.com>
2022-07-21 11:26:41 +0545

    Update Project settings structure

---
+++ AlpasProject/settings/dev.py
@@ -0,0 +1,30 @@
+import os
+from AlpasProject.settings.base import *
+
+DEBUG = os.environ.get('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
+CUSTOM_APPS = [
+    'user',
+    'core'
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+
+INSTALLED_APPS += CUSTOM_APPS
+INSTALLED_APPS += THIRD_PARTY_APPS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
