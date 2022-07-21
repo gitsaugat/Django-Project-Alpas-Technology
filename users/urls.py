ac93a874ad60b6c88c868157df8fba741bab6aa1
gitsaugat <letnotsay@gmail.com>
2022-07-21 15:34:47 +0545

    Update Templates

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name="register_view"),
     path('logout/', LogoutView.as_view(), name="logout_view"),
     path('reset/password/', password_reset_request, name="password_reset_view"),
-    path('reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/email_sent.html"),
          name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/reset.html"),
          name='password_reset_confirm'),
-    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/reset_done.html"),
          name='password_reset_complete'),
 
 
